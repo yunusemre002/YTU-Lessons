35c1056a2c746120ab7f32998af8f345e6486ee3
Yunus Emre Demir <41540426+yunusemre002@users.noreply.github.com>
2020-05-24 00:56:53 +0300

    shape changed.

--- Big Data/sumOfReviews.java
+++ Big Data/sumOfReviews.java
@@ -7,52 +7,52 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class sumOfReviews {
- 
 
- public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
- private final static IntWritable one = new IntWritable(1);
+   public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+      private final static IntWritable one = new IntWritable(1);
 
- public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {    
+      public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {    
 
-        String words[] = value.toString().split("\n"); 
-        for(String word: words )
-        {
+         String words[] = value.toString().split("\n"); 
+         for(String word: words )
+         {
             context.write(new Text("Satir"), one);
-        }
- }
- }
+         }
+      }
+   }
 
- public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
- public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
- 
-int sum = 0;
- for (IntWritable val : values) {
- sum += val.get();
- }
- context.write(key, new IntWritable(sum));
- }
- }
+      public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 
- public static void main(String[] args) throws Exception {
+         int sum = 0;
+         for (IntWritable val : values) {
+            sum += val.get();
+         }
+         context.write(key, new IntWritable(sum));
+      }
+   }
 
- Configuration conf = new Configuration();
- @SuppressWarnings("deprecation")
- Job job = new Job(conf, "sumOfReviews");
+   public static void main(String[] args) throws Exception {
 
- job.setJarByClass(sumOfReviews.class);
+      Configuration conf = new Configuration();
+      @SuppressWarnings("deprecation")
+      Job job = new Job(conf, "sumOfReviews");
 
- job.setMapperClass(Map.class);
- job.setReducerClass(Reduce.class);
+      job.setJarByClass(sumOfReviews.class);
 
- job.setOutputKeyClass(Text.class);
- job.setOutputValueClass(IntWritable.class);
+      job.setMapperClass(Map.class);
+      job.setReducerClass(Reduce.class);
 
- FileInputFormat.addInputPath(job, new Path(args[0]));
- FileOutputFormat.setOutputPath(job, new Path(args[1]));
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(IntWritable.class);
 
- job.waitForCompletion(true);
- }
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+      job.waitForCompletion(true);
+   }
  }
 
